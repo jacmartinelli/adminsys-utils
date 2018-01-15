17fd42a3432008c556e689a84fbd3a9cb7250a7e
Valentin Bourdier <valentin.bourdier@mail.com>
2018-01-15 21:38:48 +0100

    Added default value for CPU value

--- scripts/cpuwarning.sh
+++ scripts/cpuwarning.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "CPU warning limit set to '$1'"
+if [ -z "$1" ];then
+  cpu_limit="80"
+else
+  cpu_limit="$1"
+fi
+
+echo "CPU warning limit set to '$cpu_limit'"
 
 while true; do
   sleep 1
   cpuPerc=`grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}' | cut -d'.' -f1`
-  if [ "$cpuPerc" -ge "$1" ]; then
+  if [ "$cpuPerc" -ge "$cpu_limit" ]; then
     currentDate=`date "+%x %H:%M:%S"`
     echo "CPU LIMIT REACHED at $currentDate : $cpuPerc"
   fi
